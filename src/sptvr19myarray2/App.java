a470844442a7837ad722022935c8381d1cef050c
Sergei <sergei.smirnov001@ivkhk.ee>
2020-09-16 13:27:13 +0300

    Работа закончена!

--- src/sptvr19myarray2/App.java
+++ src/sptvr19myarray2/App.java
@@ -5,10 +5,40 @@
  */
 package sptvr19myarray2;
 
+import java.util.Random;
+
 /**
  *
  * @author user
  */
 class App {
+    public void run () {
+        System.out.println("--- MyArray2 ---");
+        Random random = new Random();
+        int rnum;
+        int min = 100;
+        int max = 0;
+        int sum = 0;
+        //Создаём массив чётных целых чисел.
+        int [] numbers = new int[20];
+        for (int i = 0; i < numbers.length; i++) {
+            while(true) {
+                rnum = random.nextInt(99-0+1)+0;
+                if (rnum %2 == 0) {
+                    numbers[i] = rnum;
+                    break;
+                }
+                
+            }
+            if (numbers[1] < max)max = numbers[i];
+            if (numbers[1] > min)min = numbers[i];
+            sum += numbers[i];
+            System.out.printf("%4d", numbers[i]);
+            
+        }
+        System.out.println();
+        System.out.println("Среднее арифметическое без учёта минимального(+min+) и максимального(+max+)");
+        System.out.println((double)(sum-min-max)/(numbers.length - 2));               
+    }
     
 }
